27a103ab3487cdb874fe27721b1c8a9655fc3e08
Alex Caiza <acaiza@ec.krugercorp.com>
2020-01-28 16:08:40 -0500

    config eclipse

---
+++ workspace/java-oca/src/cert/ForAmbitoVariables.java
@@ -0,0 +1,13 @@
+package src.cert;
+
+public class ForAmbitoVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < args.length; i++)
+			System.out.println(i);
+
+	}
+
+}
