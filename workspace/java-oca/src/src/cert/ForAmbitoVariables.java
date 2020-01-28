0eee4488440aacdaf19a120786ce321203008ccc
Alex Caiza <acaiza@ec.krugercorp.com>
2020-01-28 16:11:56 -0500

    config compile

--- workspace/java-oca/src/src/cert/ForAmbitoVariables.java
+++ workspace/java-oca/src/src/cert/ForAmbitoVariables.java
@@ -5,8 +5,10 @@ public class ForAmbitoVariables {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i = 0; i < args.length; i++)
+		for (int i = 0; i < 20; i++)
 			System.out.println(i);
+			//System.out.println(i);
+		//Error de compilacion i es una variable del ambito del for
 
 	}
 
