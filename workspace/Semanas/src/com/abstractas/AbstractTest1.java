f3201217b65701eb0fbad08f2ed0e142fae3afaa
Alex Caiza <alex.raul.caiza.morillo@gmail.com>
2020-02-01 21:26:12 -0500

    config tests

---
+++ workspace/Semanas/src/com/abstractas/AbstractTest1.java
@@ -0,0 +1,26 @@
+package com.abstractas;
+
+abstract class Planet{
+	
+	protected void resolve()  {		
+	}
+	
+	abstract void rotate();
+}
+
+class Earth extends Planet {
+	
+	protected void resolve() {		
+	}
+	
+	public void rotate() {		
+	}
+}
+
+public class AbstractTest1 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
