160db8ce1de45dec7edf18f16bc16c1620af0083
Alex Caiza <acaiza@ec.krugercorp.com>
2020-02-12 08:45:38 -0500

    config oca

---
+++ workspace/java-oca/src/com/arreglos/Arreglos_Asignacion.java
@@ -0,0 +1,18 @@
+package com.arreglos;
+
+public class Arreglos_Asignacion {
+    
+    public static void main(String[] args) {
+        
+        int[] arrayInt;
+        int[] multiDimArr[];
+        arrayInt = new int[5]; //valid
+        arrayInt = new int[2*5]; //valid
+        arrayInt = new int[2.67]; //invalid
+        multiDimArr = new int[5][6]; //valid
+        multiDimArr = new int[5][]; //valid
+        mutliDimArr = new int[][5]; //compilation fails
+        multiDimArr = new int[7]; //invalid
+    }
+
+}
---
+++ workspace/java-oca/src/com/arreglos/Arreglos_Declaraciones1.java
@@ -0,0 +1,22 @@
+package com.arreglos;
+
+public class Arreglos_Declaraciones1 {
+    
+    public static void main(String[] args) {
+        
+        int[] array1 = new int[10];
+        array1[0] = 1;
+        array1[1] = 2;
+        
+        int[] array2 = new int[] {1,2,3};
+        
+        int[] array2_1 = {1,2,3};
+                
+        String[] strArray = new String[] {"Jack", "Joe"};
+        
+        String strArr[] = {"Jack", "Joe"};
+        
+        int[][] arrMulti = {{1,2}, {3,4}};
+    }
+
+}
