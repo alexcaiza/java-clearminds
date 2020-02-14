18e1c5c831cd6984954a3c713031def4da391855
Alex Caiza <acaiza@ec.krugercorp.com>
2020-02-14 16:52:42 -0500

    config libros

---
+++ workspace/java-oca/src/com/herencia/sobrecarga/Metodos1.java
@@ -0,0 +1,25 @@
+package com.herencia.sobrecarga;
+
+public class Metodos1 {
+    
+    void mt(int s) {        
+    }
+    
+    int mt(int s) {        
+        return 0;
+    }
+    
+    int mt(long s) {
+        return 0;
+    }
+    
+    public int mt(Long s) {
+        return 0;
+    }
+    
+    int mt() {
+        return 0;
+    }
+    
+    
+}
